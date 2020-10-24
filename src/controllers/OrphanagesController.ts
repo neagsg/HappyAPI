0fc3b3e6ebabf43699ef20e1026d7c84dfa1443a
neagsg <geansg4@gmail.com>
2020-10-24 14:58:36 -0300

    Adicionado views

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -2,14 +2,18 @@ import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 
 import Orphanages from '../models/Orphanages';
+import orphanageView from '../views/orphanages_view';
 
 class OrphanagesController {
   async index(request: Request, response: Response) {
     const orphanagesRepository = getRepository(Orphanages);
 
-    const orphanages = await orphanagesRepository.find()
+    const orphanages = await orphanagesRepository.find({
+      relations: ['images'],
+    })
 
-    return response.json(orphanages);
+    return response.json(orphanageView.renderMany(orphanages));
+
 
   }
 
@@ -17,9 +21,11 @@ class OrphanagesController {
     const { id } = request.params;
     const orphanagesRepository = getRepository(Orphanages);
 
-    const orphanage = await orphanagesRepository.findOneOrFail(id)
+    const orphanage = await orphanagesRepository.findOneOrFail(id, {
+      relations: ['images'],
+    })
 
-    return response.json(orphanage);
+    return response.json(orphanageView.render(orphanage));
 
   }
 
